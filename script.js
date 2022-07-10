a89bd6f75759b311ad4ad5a670698640ef4d580a
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2022-07-09 20:33:30 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//portfolio
+
 let nav = document.querySelector("nav")
 
 window.onscroll = () => {
@@ -7,6 +9,36 @@ window.onscroll = () => {
         nav.classList.remove('scrolled')
     }
 }
+
+//link page
+
+const shareBtn = document.querySelector('.share-button')
+const modal = document.querySelector('.modal-container')
+const modalClose = document.querySelector('.fa-xmark')
+
+const title = window.document.title
+const url = window.document.location.href
+
+shareBtn.addEventListener('click', () => {
+    if (navigator.share) {
+        navigator.share({
+            title: title,
+            url: url
+        }).then(() => {
+            console.log('Thanks for sharing')
+        })
+    } else {
+        modal.classList.add('active')
+        modalClose.addEventListener('click', () => {
+            modal.classList.remove('active')
+        })
+        window.addEventListener("click", function(event) {
+            if (event.target == modal) {
+                modal.classList.remove("active")
+            }
+        })
+    }
+})
 
 
 
